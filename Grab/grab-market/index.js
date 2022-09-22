06459524ebce307175c161b15f4135e2d04b6a47
billy5982 <kuu0404@naver.com>
2022-09-22 22:29:54 +0900

    반응형 연습중

--- Grab/grab-market/index.js
+++ Grab/grab-market/index.js
@@ -2,8 +2,99 @@
 axios
   .get("https://8bce70f8-579d-4b27-a9f2-bf7f4fbcec6a.mock.pstmn.io/products")
   .then((res) => {
-    console.log(res);
-    const products = res.data.products;
+    const products = [
+      {
+        name: "농구공",
+        price: 10000,
+        seller: "조던",
+        imageUrl: "./images/products/basketball1.jpeg",
+      },
+      {
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "./images/products/soccerball1.jpg",
+      },
+      {
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "./images/products/keyboard1.jpg",
+      },
+      {
+        name: "농구공",
+        price: 10000,
+        seller: "조던",
+        imageUrl: "./images/products/basketball1.jpeg",
+      },
+      {
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "./images/products/soccerball1.jpg",
+      },
+      {
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "./images/products/keyboard1.jpg",
+      },
+      {
+        name: "농구공",
+        price: 10000,
+        seller: "조던",
+        imageUrl: "./images/products/basketball1.jpeg",
+      },
+      {
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "./images/products/soccerball1.jpg",
+      },
+      {
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "./images/products/keyboard1.jpg",
+      },
+      {
+        name: "농구공",
+        price: 10000,
+        seller: "조던",
+        imageUrl: "./images/products/basketball1.jpeg",
+      },
+      {
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "./images/products/soccerball1.jpg",
+      },
+      {
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "./images/products/keyboard1.jpg",
+      },
+      {
+        name: "농구공",
+        price: 10000,
+        seller: "조던",
+        imageUrl: "./images/products/basketball1.jpeg",
+      },
+      {
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "./images/products/soccerball1.jpg",
+      },
+      {
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "./images/products/keyboard1.jpg",
+      },
+    ];
+
     let inHTML = "";
 
     for (let el of products) {
